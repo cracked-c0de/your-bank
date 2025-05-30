e031c1a8568d0d00eb423ff7b784fb284b26b664
Oybek <itpustudent@gmail.com>
2025-05-30 16:34:01 +0500

    GH-Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home"
 
 function App() {
   return(
-    <BrowserRouter>
+    <BrowserRouter basename="/your-bank">
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
